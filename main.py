a8bd8d713f360ec5eb2d0631d8bf14358fecb61f
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-18 12:55:57 +0200

    'main.py' shows off the package

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
+from src.pfmp.small import *
+
 if __name__ == "__main__":
-    print("Start of tests")
-    test_are_numbers()
-    test_are_strings()
-    test_check_are_numbers()
-    test_check_different()
-    test_check_equal()
-    test_check_is_number()
-    test_check_is_probability()
-    test_check_is_string()
-    test_divide_safely()
-    test_is_dividable_by_three()
-    test_is_even()
-    test_is_number()
-    test_is_odd()
-    test_is_probability()
-    test_is_string()    
-    test_is_zero()
-    
-    # Show the documentation
+    print("Show the documentation:")
     print(is_zero.__doc__)
 
-    # Show an exception
+    print("Show how an exception looks like:")
     try:
         is_zero("should be a number")
     except TypeError as e:
         print(e)
-    
-    print("All tests passed")
+
+    print("Done showing off this package")
