b9bd80a64c22ccc83499ea5949222b42f083f555
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-19 17:40:16 +0200

    Fix style

--- tests/test_medium.py
+++ tests/test_medium.py
@@ -1,4 +1,5 @@
 import os.path
+from pathlib import Path
 import unittest
 
 from src.pfmp_richelbilderbeek.medium import (
@@ -19,134 +20,133 @@ from src.pfmp_richelbilderbeek.medium import (
     stupid_sort,
 )
 
+class TestMedium(unittest.TestCase): # noqa: D101
 
-class TestMedium(unittest.TestCase):
+    def test_are_functions(self): # noqa: D102
+        self.assertIsNotNone(are_functions.__doc__)
+        self.assertTrue(are_functions([silly_sort, stupid_sort]))
+        self.assertTrue(are_functions([silly_sort]))
+        self.assertFalse(are_functions([]))
+        self.assertFalse(are_functions(3.14))
+        self.assertFalse(are_functions("Hello"))
 
-    def test_are_functions(self):
-        assert are_functions.__doc__ is not None
-        assert are_functions([silly_sort, stupid_sort])
-        assert are_functions([silly_sort])
-        assert not are_functions([])
-        assert not are_functions(3.14)
-        assert not are_functions("Hello")
-
-    def test_are_speed_measurements(self):
-        assert are_speed_measurements.__doc__ is not None
-        assert are_speed_measurements(get_test_speed_measurements())
-        assert not are_speed_measurements(0.1)
-        assert not are_speed_measurements({"x": 1.0})
-        assert not are_speed_measurements({"x": 1.0, "y": 2.0, "z": 3.0})
+    def test_are_speed_measurements(self): # noqa: D102
+        self.assertIsNotNone(are_speed_measurements.__doc__)
+        self.assertTrue(are_speed_measurements(get_test_speed_measurements()))
+        self.assertFalse(are_speed_measurements(0.1))
+        self.assertFalse(are_speed_measurements({"x": 1.0}))
+        self.assertFalse(are_speed_measurements({"x": 1.0, "y": 2.0, "z": 3.0}))
         nasty_data_1 = {
             "function_index": [0, 1, 234567], # Oops
             "data_length": [0, 0],
             "runtime_speed_secs": [0.1, 0.2],
         }
-        assert not are_speed_measurements(nasty_data_1)
+        self.assertFalse(are_speed_measurements(nasty_data_1))
         nasty_data_2 = {
             "function_index": [0, 1],
             "data_length": [0, 0, 123456789], # Oops
             "runtime_speed_secs": [0.1, 0.2],
         }
-        assert not are_speed_measurements(nasty_data_2)
+        self.assertFalse(are_speed_measurements(nasty_data_2))
         nasty_data_3 = {
             "function_index": [0, 1],
             "data_length": [0, 0],
             "runtime_speed_secs": [0.1, 0.2, 0.3456789], # Oops
         }
-        assert not are_speed_measurements(nasty_data_3)
+        self.assertFalse(are_speed_measurements(nasty_data_3))
 
-    def test_get_datas(self):
-        assert get_datas.__doc__ is not None
-        assert is_list(get_datas())
-        assert get_datas(42) == get_datas(42)
-        assert get_datas(42) != get_datas(43)
+    def test_get_datas(self): # noqa: D102
+        self.assertIsNotNone(get_datas.__doc__)
+        self.assertTrue(is_list(get_datas()))
+        self.assertEqual(get_datas(42), get_datas(42))
+        self.assertNotEqual(get_datas(42), get_datas(43))
 
-    def test_get_sorting_functions(self):
-        assert get_sorting_functions.__doc__ is not None
-        assert are_functions(get_sorting_functions())
+    def test_get_sorting_functions(self): # noqa: D102
+        self.assertIsNotNone(get_sorting_functions.__doc__)
+        self.assertTrue(are_functions(get_sorting_functions()))
 
-    def test_get_speed_measurements(self):
-        assert get_speed_measurements.__doc__ is not None
+    def test_get_speed_measurements(self): # noqa: D102
+        self.assertIsNotNone(get_speed_measurements.__doc__)
         speed_measurements = get_speed_measurements(
             datas = get_test_datas(),
             functions = get_sorting_functions(),
         )
-        assert are_speed_measurements(speed_measurements)
+        self.assertTrue(are_speed_measurements(speed_measurements))
 
-    def test_get_test_datas(self):
-        assert get_test_datas.__doc__ is not None
-        assert is_list(get_test_datas())
-        assert get_test_datas(42) == get_test_datas(42)
-        assert get_test_datas(42) != get_test_datas(43)
+    def test_get_test_datas(self): # noqa: D102
+        self.assertIsNotNone(get_test_datas.__doc__)
+        self.assertTrue(is_list(get_test_datas()))
+        self.assertEqual(get_test_datas(42), get_test_datas(42))
+        self.assertNotEqual(get_test_datas(42), get_test_datas(43))
 
-    def test_get_test_speed_measurements(self):
-        assert get_test_speed_measurements.__doc__ is not None
-        assert isinstance(get_test_speed_measurements(), dict)
+    def test_get_test_speed_measurements(self): # noqa: D102
+        self.assertIsNotNone(get_test_speed_measurements.__doc__)
+        self.assertTrue(isinstance(get_test_speed_measurements(), dict))
 
-    def test_is_dict(self):
-        assert is_dict.__doc__ is not None
-        assert is_dict({"X": 3.14})
-        assert not is_dict(3.14)
-        assert not is_dict([3.14])
+    def test_is_dict(self): # noqa: D102
+        self.assertIsNotNone(is_dict.__doc__)
+        self.assertTrue(is_dict({"X": 3.14}))
+        self.assertFalse(is_dict(3.14))
+        self.assertFalse(is_dict([3.14]))
 
-    def test_is_function(self):
-        assert is_function.__doc__ is not None
-        assert is_function(is_function)
-        assert not is_function(42)
+    def test_is_function(self): # noqa: D102
+        self.assertIsNotNone(is_function.__doc__)
+        self.assertTrue(is_function(is_function))
+        self.assertFalse(is_function(42))
 
-    def test_is_list(self):
-        assert is_list.__doc__ is not None
-        assert not is_list(42)
+    def test_is_list(self): # noqa: D102
+        self.assertIsNotNone(is_list.__doc__)
+        self.assertFalse(is_list(42))
 
 
-    def test_is_sorted(self):
-        assert is_sorted.__doc__ is not None
-        assert is_sorted([1, 2])
-        assert is_sorted([1])
-        assert not is_sorted([2, 1])
-        assert not is_sorted(get_datas()[0])
+    def test_is_sorted(self): # noqa: D102
+        self.assertIsNotNone(is_sorted.__doc__)
+        self.assertTrue(is_sorted([1, 2]))
+        self.assertTrue(is_sorted([1]))
+        self.assertFalse(is_sorted([2, 1]))
+        self.assertFalse(is_sorted(get_datas()[0]))
 
-    def test_save_dict(self):
-        assert save_dict.__doc__ is not None
+    def test_save_dict(self): # noqa: D102
+        self.assertIsNotNone(save_dict.__doc__)
         csv_filename = "temp_save_dict.csv"
         if (os.path.isfile(csv_filename)):
             os.remove(csv_filename)
-        assert not os.path.isfile(csv_filename)
+        self.assertFalse(os.path.isfile(csv_filename))
 
         save_dict(
             x = { "X": [1, 2, 3], "Y": [4, 5, 6]},
             csv_filename = csv_filename,
         )
-        assert os.path.isfile(csv_filename)
+        self.assertTrue(os.path.isfile(csv_filename))
         os.remove(csv_filename)
 
-    def test_save_speed_measurements(self):
-        assert save_speed_measurements.__doc__ is not None
+    def test_save_speed_measurements(self): # noqa: D102
+        self.assertIsNotNone(save_speed_measurements.__doc__)
         csv_filename = "temp_save_speed_measurements.csv"
         if (os.path.isfile(csv_filename)):
             os.remove(csv_filename)
-        assert not os.path.isfile(csv_filename)
+        self.assertFalse(os.path.isfile(csv_filename))
         save_speed_measurements(
             speed_measurements = get_test_speed_measurements(),
             csv_filename = csv_filename,
         )
-        assert os.path.isfile(csv_filename)
-        os.remove(csv_filename)
+        self.assertTrue(os.path.isfile(csv_filename))
+        Path.unlink(csv_filename)
 
 
 
-    def test_silly_sort(self):
-        assert silly_sort.__doc__ is not None
+    def test_silly_sort(self): # noqa: D102
+        self.assertIsNotNone(silly_sort.__doc__)
         data = get_datas()[0]
-        assert not is_sorted(data)
+        self.assertFalse(is_sorted(data))
         sorted_data = silly_sort(data)
-        assert is_sorted(sorted_data)
+        self.assertTrue(is_sorted(sorted_data))
 
 
-    def test_stupid_sort(self):
-        assert stupid_sort.__doc__ is not None
+    def test_stupid_sort(self): # noqa: D102
+        self.assertIsNotNone(stupid_sort.__doc__)
         data = get_datas()[0]
-        assert not is_sorted(data)
+        self.assertFalse(is_sorted(data))
         sorted_data = stupid_sort(data)
-        assert is_sorted(sorted_data)
+        self.assertTrue(is_sorted(sorted_data))
 
--- tests/test_small.py
+++ tests/test_small.py
@@ -1,3 +1,4 @@
+"""Tests all function in src.pfmp_richelbilderbeek.small."""
 import unittest
 
 from src.pfmp_richelbilderbeek.small import (
@@ -20,26 +21,26 @@ from src.pfmp_richelbilderbeek.small import (
 )
 
 
-class TestSmall(unittest.TestCase):
-    def test_are_numbers(self):
-        assert are_numbers.__doc__ is not None
-        assert not are_numbers(":-/")
-        assert are_numbers([1, 2])
-        assert are_numbers([1.1])
-        assert not are_numbers([])
-        assert not are_numbers(["1.2"])
+class TestSmall(unittest.TestCase): # noqa: D101
+    def test_are_numbers(self: "TestSmall"): # noqa: D102
+        self.assertIsNotNone(are_numbers.__doc__)
+        self.assertFalse(are_numbers(":-/"))
+        self.assertTrue(are_numbers([1, 2]))
+        self.assertTrue(are_numbers([1.1]))
+        self.assertFalse(are_numbers([]))
+        self.assertFalse(are_numbers(["1.2"]))
 
-    def test_are_strings(self):
-        assert are_strings.__doc__ is not None
-        assert are_strings(["A"])
-        assert are_strings(["A", "B"])
-        assert not are_strings("A")
-        assert not are_strings(3.14)
-        assert not are_strings(["A", 3.14])
-        assert not are_strings([])
+    def test_are_strings(self): # noqa: D102
+        self.assertIsNotNone(are_strings.__doc__)
+        self.assertTrue(are_strings(["A"]))
+        self.assertTrue(are_strings(["A", "B"]))
+        self.assertFalse(are_strings("A"))
+        self.assertFalse(are_strings(3.14))
+        self.assertFalse(are_strings(["A", 3.14]))
+        self.assertFalse(are_strings([]))
 
-    def test_check_are_numbers(self):
-        assert check_are_numbers.__doc__ is not None
+    def test_check_are_numbers(self): # noqa: D102
+        self.assertIsNotNone(check_are_numbers.__doc__)
         check_are_numbers([3.14])
         check_are_numbers([3.14, 42])
         has_thrown = False
@@ -47,10 +48,10 @@ class TestSmall(unittest.TestCase):
             check_are_numbers("A")
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_check_different(self):
-        assert check_different.__doc__ is not None
+    def test_check_different(self): # noqa: D102
+        self.assertIsNotNone(check_different.__doc__)
         check_different(1.2, 1.3)
         check_different(1.2, "A")
 
@@ -59,17 +60,17 @@ class TestSmall(unittest.TestCase):
             check_different(1.1, 1.1)
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             check_different("1.1", "1.1")
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_check_equal(self):
-        assert check_equal.__doc__ is not None
+    def test_check_equal(self): # noqa: D102
+        self.assertIsNotNone(check_equal.__doc__)
         check_equal(1.2, 1.2)
         check_equal("A", "A")
 
@@ -78,17 +79,17 @@ class TestSmall(unittest.TestCase):
             check_equal(1.1, 1.2)
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             check_equal(1.1, "1.1")
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_check_is_number(self):
-        assert check_is_number.__doc__ is not None
+    def test_check_is_number(self): # noqa: D102
+        self.assertIsNotNone(check_is_number.__doc__)
         check_is_number(1.2)
 
         has_thrown = False
@@ -96,17 +97,17 @@ class TestSmall(unittest.TestCase):
             check_is_number( [1.1, 1.2] )
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             check_is_number("1.1")
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_check_is_probability(self):
-        assert check_is_probability.__doc__ is not None
+    def test_check_is_probability(self): # noqa: D102
+        self.assertIsNotNone(check_is_probability.__doc__)
         check_is_probability(0.2)
 
         has_thrown = False
@@ -114,24 +115,24 @@ class TestSmall(unittest.TestCase):
             check_is_probability( [0.1, 0.2] )
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             check_is_probability("0.1")
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             check_is_probability(123.456)
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_check_is_string(self):
-        assert check_is_string.__doc__ is not None
+    def test_check_is_string(self): # noqa: D102
+        self.assertIsNotNone(check_is_string.__doc__)
         check_is_string("A")
 
         has_thrown = False
@@ -139,102 +140,102 @@ class TestSmall(unittest.TestCase):
             check_is_string( ["A", "B"] )
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             check_is_string(3.14)
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_divide_safely(self):
-        assert divide_safely.__doc__ is not None
-        assert divide_safely(1.2, 0.3) > 0.0
+    def test_divide_safely(self): # noqa: D102
+        self.assertIsNotNone(divide_safely.__doc__)
+        self.assertTrue(divide_safely(1.2, 0.3) > 0.0) # noqa: PLR2004
 
         has_thrown = False
         try:
             divide_safely(1.1, 0.0)
         except RuntimeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
 
-    def test_is_dividable_by_three(self):
-        assert is_dividable_by_three.__doc__
-        assert not is_dividable_by_three(2)
+    def test_is_dividable_by_three(self): # noqa: D102
+        self.assertIsNotNone(is_dividable_by_three)
+        self.assertFalse(is_dividable_by_three(2))
 
         has_thrown = False
         try:
             is_dividable_by_three(3.14)
         except TypeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_is_even(self):
-        assert is_even.__doc__
-        assert not is_even(1)
+    def test_is_even(self): # noqa: D102
+        self.assertIsNotNone(is_even.__doc__)
+        self.assertFalse(is_even(1))
 
         has_thrown = False
         try:
             is_even(3.14)
         except TypeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
 
-    def test_is_number(self):
-        assert is_number.__doc__
-        assert is_number(42)
-        assert is_number(3.14)
-        assert not is_number("a string")
+    def test_is_number(self): # noqa: D102
+        self.assertIsNotNone(is_number.__doc__)
+        self.assertTrue(is_number(42))
+        self.assertTrue(is_number(3.14))
+        self.assertFalse(is_number("a string"))
 
-    def test_is_odd(self):
-        assert is_odd.__doc__
-        assert not is_odd(2)
+    def test_is_odd(self): # noqa: D102
+        self.assertIsNotNone(is_odd.__doc__)
+        self.assertFalse(is_odd(2))
 
         has_thrown = False
         try:
             is_odd(3.14)
         except TypeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_is_probability(self):
-        assert is_probability.__doc__
-        assert is_probability(0.1)
-        assert not is_probability(1.2)
-        assert not is_probability(-1.2)
+    def test_is_probability(self): # noqa: D102
+        self.assertIsNotNone(is_probability.__doc__)
+        self.assertTrue(is_probability(0.1))
+        self.assertFalse(is_probability(1.2))
+        self.assertFalse(is_probability(-1.2))
 
         has_thrown = False
         try:
             is_probability("I am a string")
         except TypeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
-    def test_is_string(self):
-        assert is_string("Hello")
-        assert not is_string({"Hello", "too much strings"})
-        assert is_string.__doc__
+    def test_is_string(self): # noqa: D102
+        self.assertIsNotNone(is_string.__doc__)
+        self.assertTrue(is_string("Hello"))
+        self.assertFalse(is_string({"Hello", "too much strings"}))
 
 
-    def test_is_zero(self):
-        assert is_zero.__doc__
-        assert is_zero(0)
-        assert is_zero(0.0)
-        assert not is_zero(1)
+    def test_is_zero(self): # noqa: D102
+        self.assertIsNotNone(is_zero.__doc__)
+        self.assertTrue(is_zero(0))
+        self.assertTrue(is_zero(0.0))
+        self.assertFalse(is_zero(1))
 
         has_thrown = False
         try:
             is_zero( { 1, 2 } )
         except TypeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
 
         has_thrown = False
         try:
             is_zero("I am a string")
         except TypeError:
             has_thrown = True
-        assert has_thrown
+        self.assertTrue(has_thrown)
