f97ba5f6cb9a085b470daafd10540ce370257fd0
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-26 10:47:14 +0200

    Add questions

--- src/pfmp_richelbilderbeek/easy_questions.py
+++ src/pfmp_richelbilderbeek/easy_questions.py
@@ -5,24 +5,18 @@
  * no for-loops
  * maximally one variable modified
 
-Name                          |Purpose
-------------------------------|-----------------------------------------------------------------------
-`is_even(x)`                  |Returns `True` if `x` is even
-`is_odd(x)`                   |Returns `True` if `x` is odd
-`is_probability(p)`           |Returns `True` if `p` is a probability
-                              |(i.e. a chance of something happening)
-`is_zero(x)`                  |Returns `True` if `x` is zero
+0: `is_zero(x)`
+1: `is_even(x)`
+2: `is_odd(x)`
+3: `is_probability(p)`
+4: `is_number(x)`
+5: `are_numbers(x)`
+S1: `is_prime(x)`
 
-Extra:
+"""
 
-Name                          |Purpose
-------------------------------|-----------------------------------------------------------------------
-`are_numbers(x)`              |Returns `True` if `x` is zero, one or more numbers
-`are_strings(x)`              |Returns `True` if `x` is zero, one or more strings
-`is_dividable_by_three(x)`    |Returns `True` if `x` is dividable by 3
-`is_number(x)`                |Returns `True` if `x` is a number
-`is_string(x)`                |Returns `True` if `s` is a string
-
+"""
+Example code
 """
 
 def is_zero(x):
@@ -39,3 +33,60 @@ def is_zero(x):
             "Actual type of 'number': ", type(x),
         )
     return x == 0
+
+"""Exercise 1: develop the function 'is_even'.
+
+In this context, 'even', means 'dividable by two', e.g. 2, 4, -6, 0.
+
+-   Create a GitHub repository called `is_even`
+-   Develop a function called `is_even`
+-   Try to be **exemplary**
+"""
+
+"""Exercise 2: develop the function 'is_odd' 
+
+In this context, 'odd', means 'not dividable by two', e.g. 1, 3, -5.
+
+-   Create a GitHub repository called `is_odd`
+-   Develop a function called `is_odd`
+-   Try to be **exemplary**
+"""
+
+"""Exercise 3: develop the function 'is_probability'.
+
+In this context: the change of something happening; a value between zero and one, including zero and one.
+
+-   Create a GitHub repository called `is_probability`
+-   Develop a function called `is_probability`
+-   Try to be **exemplary**
+"""
+
+"""Exercise 4: develop the function 'is_number'.
+
+This function determines if a number is a number.
+
+-   Create a GitHub repository called `is_number`
+-   Develop a function called `is_number`
+-   Try to be **exemplary**
+"""
+
+"""Exercise 5: develop the function 'are_numbers'.
+
+This function determines if the input are numbers [*], 
+similar to `is_number`, but for more numbers.
+
+-   Create a GitHub repository called `are_numbers`
+-   Develop a function called `are_numbers`
+-   Try to be **exemplary**
+
+[*] vague on purpose
+"""
+
+"""Exercise S1: develop the function 'is_prime'.
+
+This function determines if a number is a prime number.
+
+-   Create a GitHub repository called `is_prime`
+-   Develop a function called `is_prime`
+-   Try to be **exemplary**
+"""
---
+++ src/pfmp_richelbilderbeek/medium_questions.py
@@ -0,0 +1,23 @@
+"""General-purpose functions of moderate complexity.
+
+Moderate complexity' means:
+ * a cyclomatic complexity of 1 to 8
+ * may have for-loops
+ * maximally two variables modified
+"""
+
+def is_prime(x):
+    """Determine if `x` is prime.
+
+    Returns True if `x` is an integer that is prime.
+    Returns False if `x` is an integer that is not prime.
+    Raises a TypeError if `x` is not an integer
+    """
+    if not isinstance(x, int):
+        message = "'x' must be an integer"
+        raise TypeError(message)
+    first_prime = 2
+    if x < first_prime:
+        return False
+    return all(x % i != 0 for i in range(first_prime, x))
+
